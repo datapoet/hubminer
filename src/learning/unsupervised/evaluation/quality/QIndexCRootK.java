4ca938e7e09c649c971fbda4f019c80c1cb799b9
datapoet <nenad.tomasev@gmail.com>
2015-01-06 11:20:44 +0100

    Improved noise handling in CRootK cl. qual. index.

--- src/learning/unsupervised/evaluation/quality/QIndexCRootK.java
+++ src/learning/unsupervised/evaluation/quality/QIndexCRootK.java
@@ -84,7 +84,8 @@ public class QIndexCRootK extends ClusteringQualityIndex {
                     Math.max(20, clusterAssociations.length / numClusters));
         }
         for (int i = 0; i < dset.size(); i++) {
-            if (clusterAssociations[i] >= 0) {
+            if (clusterAssociations[i] >= 0 &&
+                    !dset.getInstance(i).isNoise()) {
                 allNonNoisyIndexes.add(i);
                 clusterIndexes[clusterAssociations[i]].add(i);
             }
